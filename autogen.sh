a1580a592d97cd45d8591ac840dc3ecf142673f8
Mike Gorse <mgorse@suse.com>
2016-03-04 15:31:57 -0600

    Reorganize files to prepare for i18n

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+intltoolize --force --copy --automake || exit 1
+
+# gnome-autogen.sh runs configure, so do likewise.
+autoreconf -vif
+
+cd "$olddir"
+
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
---
+++ include.mk
@@ -0,0 +1,8 @@
+gschemabase = org.gnome.shell.extensions
+topextensiondir = $(datadir)/gnome-shell/extensions
+extensionname = hibernate-status
+uuid = $(extensionname)@dromi
+extensiondir = $(topextensiondir)/$(uuid)
+localextensiondir = $(HOME)/.local/share/gnome-shell/extensions/$(uuid)
+extensionurl = https://github.com/arelange/gnome-shell-extension-hibernate-status
+version = 7
